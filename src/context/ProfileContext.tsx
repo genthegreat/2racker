be7ef98f62d373484370f5d9661e320a6424c009
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-04-11 18:07:16 +0200

    fix lint error

--- src/context/ProfileContext.tsx
+++ src/context/ProfileContext.tsx
@@ -1,65 +1,66 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { createClient } from '@/utils/supabase/client';
-import { User } from '@supabase/supabase-js';
+import { UserResponse } from '@supabase/supabase-js';
 
 interface Profile {
-    fullname: string | null;
-    username: string | null;
-    avatarUrl: string | null;
-  }
-  
-  interface ProfileContextData {
-    profile: Profile;
-    loading: boolean;
-    error: any | null;
-  }
-  
-  const ProfileContext = createContext<ProfileContextData>({
-    profile: { fullname: null, username: null, avatarUrl: null },
-    loading: false,
-    error: null,
-  });
-  
-  export const ProfileProvider = ({ children }: { children: React.ReactNode }) => {
-    const supabase = createClient()
-    const [profile, setProfile] = useState<Profile>({ fullname: null, username: null, avatarUrl: null });
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<any | null>(null);
-  
-    const fetchProfile = async (userId: string) => {
-      setLoading(true);
-      try {
-        const { data, error, status } = await supabase
-          .from('profiles')
-          .select('full_name, username, avatar_url')
-          .eq('id', userId)
-          .single();
-  
-        if (error && status !== 406) throw error;
-  
-        if (data) {
-          setProfile(data);
-        }
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
+  full_name: string | null;
+  username: string | null;
+  avatar_url: string | null;
+}
+
+interface ProfileContextData {
+  profile: Profile;
+  loading: boolean;
+  error: any | null;
+}
+
+const ProfileContext = createContext<ProfileContextData>({
+  profile: { full_name: null, username: null, avatar_url: null },
+  loading: false,
+  error: null,
+});
+
+export const ProfileProvider = ({ children }: { children: React.ReactNode }) => {
+  const supabase = createClient()
+  const [profile, setProfile] = useState<Profile>({ full_name: null, username: null, avatar_url: null });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any | null>(null);
+
+  const fetchProfile = async (userId: string) => {
+    setLoading(true);
+    try {
+      const { data, error, status } = await supabase
+        .from('profiles')
+        .select('full_name, username, avatar_url')
+        .eq('id', userId)
+        .single();
+
+      if (error && status !== 406) throw error;
+
+      if (data) {
+        setProfile(data);
       }
-    };
-  
-    useEffect(() => {
-      // Fetch profile data if you have a user.id available here
-      const user = supabase.auth.getUser(); 
-      if (user) {
-        fetchProfile(user.id);
-      }
-    }, []);
-  
-    return (
-      <ProfileContext.Provider value={{ profile, loading, error }}>
-        {children}
-      </ProfileContext.Provider>
-    );
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
-  
+
+  useEffect(() => {
+    supabase.auth.getUser().then((response: UserResponse) => {
+        if (response.data?.user) { // Check if user exists within data
+            fetchProfile(response.data.user.id); 
+        }
+    });
+}, []);
+
+
+  return (
+    <ProfileContext.Provider value={{ profile, loading, error }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
 export const useProfileContext = () => useContext(ProfileContext);
