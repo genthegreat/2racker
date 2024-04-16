c11ded733bf918c67445bf4f53e374e11440bec9
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-04-16 12:14:13 +0200

    added projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,89 +6,96 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { useState, useEffect } from "react"
 import { formatCurrency } from '../utils'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 
-interface Project {
+interface Amenity {
   project_name?: any;
 }
 
-interface Account {
-  account_id: any;
-  account_name: any;
-  status: any;
+interface Project {
+  project_id: any;
+  project_name: any;
+  description: any;
   amount_due: any;
   amount_paid: any;
   balance: any;
-  start_date?: any;
   projects?: Project[];
 }
 
-export default function Account() {
+export default function Project() {
   const supabase = createClientComponentClient()
-  const [accounts, setAccounts] = useState<Account[]>([])
+  const searchParams = useSearchParams()
 
-  const getAccounts = async() => {
+  const id = searchParams.get('id')
+
+  // State
+  const [projects, setprojects] = useState<Project[]>([])
+
+  const getprojects = async() => {
     try {
       
       const { data, error } = await supabase
-        .from('accounts')
+        .from('projects')
         .select(`
-          account_id,
-          account_name,
-          status,
+          project_id,
+          project_name,
+          description,
           amount_due,
           amount_paid,
-          balance
+          balance,
+          account_id
         `)
+        .eq('account_id', `${id}`)
 
       if (error) {
-        console.log(error)
+        console.log(error.message)
         throw error
       }
       if (data) {
         console.log(data)
-        setAccounts(data)
+        setprojects(data)
       }
     } catch (error:any) {
-      console.error('Error fetching accounts:', error.message);
+      console.error('Error fetching projects:', error.message);
     }
   }
 
   useEffect(() => {
-    getAccounts()
+    getprojects()
   }, [])
 
   return (
     <div className='align-center'>
 
-      <h2 className="overline text-2xl mt-4">YOUR ACCOUNTS</h2>
+      <h2 className="overline text-2xl mt-4">YOUR PROJECTS - {id}</h2>
 
       <div>
         <table className="w-full table-auto border-separate border border-blue-600">
             <thead>
               <tr>
-                <th className="border border-green-600 px-5">Account</th>
+                <th className="border border-green-600 px-5">project</th>
                 <th className="border border-green-600 px-5">Amount Due</th>
                 <th className="border border-green-600 px-5">Amount Paid</th>
                 <th className="border border-green-600 px-5">Balance</th>
-                <th className="border border-green-600 px-5">Status</th>
+                <th className="border border-green-600 px-5">description</th>
               </tr>
             </thead>
             <tbody>
-              {accounts.map(account => (
-                <tr key={`${account.account_id}`}>
-                    <td className="border border-green-600 px-5"><Link href={`/projects?id=${account.account_id}`}>{account.account_name}</Link></td>
-                    <td className="border border-green-600 px-5">{formatCurrency(account.amount_due)}</td>
-                    <td className="border border-green-600 px-5">{formatCurrency(account.amount_paid)}</td>
-                    <td className="border border-green-600 px-5">{formatCurrency(account.balance)}</td>
-                    <td className="border border-green-600 px-5">{account.status}</td>                                 
-                  </tr> 
+              {projects.map(project => (
+                <tr key={`${project.project_id}`}>
+                  <td className="border border-green-600 px-5">{project.project_name}</td>
+                  <td className="border border-green-600 px-5">{formatCurrency(project.amount_due)}</td>
+                  <td className="border border-green-600 px-5">{formatCurrency(project.amount_paid)}</td>
+                  <td className="border border-green-600 px-5">{formatCurrency(project.balance)}</td>
+                  <td className="border border-green-600 px-5">{project.description}</td>
+                </tr>                
               ))}
             </tbody>
         </table>
       </div>
 
       <div className='flex justify-end pt-10'>
-        <Link href="/accounts/add">
+        <Link href="/projects/add">
           <button className="w-52 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add New</button>
         </Link>
       </div>
