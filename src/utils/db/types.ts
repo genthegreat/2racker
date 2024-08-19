1a725c3dfbbc0322604cf044c7589237574257e6
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-08-19 18:12:14 +0200

    Add a foreign key between transactions and account to reduce chaining

--- src/utils/db/types.ts
+++ src/utils/db/types.ts
@@ -1,49 +1,78 @@
+export interface Account {
+  account_id: number,
+  user_id?: string,
+  account_name: string,
+  status: string,
+  amount_due: number,
+  amount_paid: number,
+  balance: number,
+  start_date?: string,
+  projects?: Project[],
+  transactions?: Transaction[]
+}
+
 export interface Transaction {
   transaction_id: number,
+  account_id: number,
   amount_paid: number,
   transaction_date: string,
   platform: string,
   receipt_info: string,
   status: string,
-  notes: string,
-  amenity_id: number;
+  notes?: string,
+  amenity_id?: number,
 }
 
 export interface Amenity {
-  amenity_id: number;
-  amenity_name: string;
-  default_amount: number;
-  category: string;
-  project_id: number;
-  transactions?: Transaction[];
+  amenity_id: number,
+  amenity_name: string,
+  default_amount: number,
+  category: string,
+  project_id: number,
+  transactions?: Transaction[],
 }
 
 export interface Project {
-  project_id: number;
-  account_id: number;
-  project_name: string;
-  description: string;
-  amount_due: number;
-  amount_paid: number;
-  amenities?: Amenity[];
+  project_id: number,
+  account_id: number,
+  project_name: string,
+  description: string,
+  amount_due: number,
+  amount_paid: number,
+  amenities?: Amenity[],
 }
 
-export interface Account {
+export interface Profile {
+  id: string,
+  updated_at: string,
+  full_name: string | null,
+  username: string | null,
+  avatar_url: string | null,
+}
+
+export interface AccountDetails {
   account_id: number;
-  user_id?: string;
   account_name: string;
   status: string;
   amount_due: number;
   amount_paid: number;
   balance: number;
   start_date?: string;
-  projects?: Project[];
+  amenities: AmenityDetails[];
+  transactions: TransactionDetails[];
 }
 
-export interface Profile {
-  id: string;
-  updated_at: string;
-  full_name: string | null;
-  username: string | null;
-  avatar_url: string | null;
+export interface AmenityDetails {
+  amenity_id: number;
+  amenity_name: string;
+  default_amount: number;
+}
+
+export interface TransactionDetails {
+  transaction_id: number;
+  amount_paid: number;
+  transaction_date: string;
+  platform: string;
+  receipt_info: string;
+  status: string;
 }
