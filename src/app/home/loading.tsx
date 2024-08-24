8388c43980bf171949c697bb085a3d2d7c4e26a2
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-08-24 23:43:12 +0200

    Add loading ui

--- src/app/home/loading.tsx
+++ src/app/home/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function loading() {
   return (
-      <div>
+      <div className='flex flex-col justify-center items-center'>
           <h1 className="overline text-2xl mt-4">Welcome...</h1>
           <Spinner />
       </div>
--- src/app/login/loading.tsx
+++ src/app/login/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function loading() {
   return (
-      <div>
+      <div className='flex flex-col justify-center items-center'>
           <Spinner />
           <span>loading...</span>
       </div>
