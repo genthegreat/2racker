0cfd5d7850f43d090578a86bdabe48a49fdf17c9
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-08-31 14:56:56 +0200

    Edit home page flex

--- src/components/SingleCard.tsx
+++ src/components/SingleCard.tsx
@@ -7,35 +7,33 @@ export const SingleCard = ({
     btnHref,
 }: any) => {
     return (
-        <>
-            <div className="mb-10 overflow-hidden rounded-lg shadow-black shadow-sm duration-300 hover:shadow-3">
-                {image && (<img src={image} alt="" className="w-full" />)}
-                <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
-                    <h3 className="mb-4 block text-xl font-semibold text-dark hover:text-primary sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
-                        {titleHref ? (
-                            <a
-                                href={titleHref}
-                            >
-                                {CardTitle}
-                            </a>
-                        ) :
-                            CardTitle
-                        }
-                    </h3>
-                    <p className="mb-7 text-justify text-base sm:text-[16px] md:text-[18px] text-gray-600">
-                        {CardDescription}
-                    </p>
-
-                    {Button && (
+        <div className="overflow-hidden rounded-lg shadow-black shadow-sm duration-300 hover:shadow-md hover:shadow-slate-800 flex flex-col h-full">
+            {image && (<img src={image} alt="" className="w-full" />)}
+            <div className="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                <h3 className="mb-4 block text-xl font-semibold text-dark hover:text-primary sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]">
+                    {titleHref ? (
                         <a
-                            href={btnHref ? btnHref : "#"}
-                            className="inline-block rounded-full border border-gray-3 px-7 py-2 text-base font-medium text-body-color transition hover:border-primary hover:bg-primary hover:text-white"
+                            href={titleHref}
                         >
-                            {Button}
+                            {CardTitle}
                         </a>
-                    )}
-                </div>
+                    ) :
+                        CardTitle
+                    }
+                </h3>
+                <p className="mb-7 text-justify text-base sm:text-[16px] md:text-[18px] text-gray-600">
+                    {CardDescription}
+                </p>
+
+                {Button && (
+                    <a
+                        href={btnHref ? btnHref : "#"}
+                        className="inline-block rounded-full border border-gray-3 px-7 py-2 text-base font-medium text-body-color transition hover:border-primary hover:bg-primary hover:text-white"
+                    >
+                        {Button}
+                    </a>
+                )}
             </div>
-        </>
+        </div>
     );
 };
