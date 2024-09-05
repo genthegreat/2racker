4909a6ddf7e571a4cd542bc7a76288fdf610993e
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-09-05 23:34:54 +0200

    Declare db types globally

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -1,5 +1,10 @@
-import type { Database as DB } from "@@/utils/db/database.types"
+import type { Database as DB } from '@@/utils/db/database.types'
 
 declare global {
     type Database = DB
+    type Accounts = DB['public']['Tables']['accounts']['Row'];
+    type Amenities = DB['public']['Tables']['amenities']['Row'];
+    type Profiles = DB['public']['Tables']['profiles']['Row'];
+    type Projects = DB['public']['Tables']['projects']['Row'];
+    type Transaction = DB['public']['Tables']['transactions']['Row'];
 }
