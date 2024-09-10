3ed04b8e789b7701b41b5a391e55a3e9f4dbed48
Ezra Gonyuie <ezragonyuie@gmail.com>
2024-09-10 20:25:44 +0200

    fix link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <div>
       <div className='flex flex-col items-center justify-center py-2 bg-slate-900 mt-2 text-white'>
         <p className="my-1 flex justify-center items-center text-center divide-x divide-gray-300 dark:divide-gray-700">
-          <Link className="pr-3 inline-flex items-center gap-x-2 text-sm text-gray-600 decoration-2 hover:underline hover:text-blue-600 dark:text-gray-500 dark:hover:text-gray-200" href="/privacy">
+          <Link className="pr-3 inline-flex items-center gap-x-2 text-sm text-gray-600 decoration-2 hover:underline hover:text-blue-600 dark:text-gray-500 dark:hover:text-gray-200" href="/contact">
             Contact Us
           </Link>
           <Link className="px-3 inline-flex items-center gap-x-2 text-sm text-gray-600 decoration-2 hover:underline hover:text-blue-600 dark:text-gray-500 dark:hover:text-gray-200" href="https://princekwesi.website" target="_blank">
